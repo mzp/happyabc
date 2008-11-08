16d0645d90444927ec69feab76e0b538b567c959
mzp <mzpppp@gmail.com>
2008-11-09 00:11:29 +0900

    release script

---
+++ src.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+###
+VERSION=$(head -1 VERSION)
+FULL_VERSION=$(tail -1 VERSION)
+
+###
+function checkout(){
+    $1 $2
+    cd $2
+#    omake check || exit 1
+#    omake clean
+    cd -
+}
+echo $VERSION
+echo $FULL_VERSION
+
+dir=habc-$VERSION
+
+echo '### cleanup ###'
+rm -rf habc-*
+mkdir $dir
+
+echo '### checkout(xml) ###'
+checkout 'svn export http://www.libspark.org/svn/ocaml/abc2xml/trunk' ${dir}/xml || exit 1
+
+echo '### checkout(core) ###'
+checkout 'git clone git://github.com/mzp/scheme-abc.git --depth=0' ${dir}/core || exit 1
+rm -rf ${dir}/core/.git
+
+echo '### copy driver ###'
+sed "s/<Version>/${FULL_VERSION}/" habc > ${dir}/habc
+cp extra/* ${dir}
+
+tar cvjf ${dir}.tar.bz2 ${dir}
