6cf3f7126a2ae829240f6b87cd20581af5f9d68c
mzp <mzpppp@gmail.com>
2008-11-09 10:27:37 +0900

    version 0.1.0

--- src.sh
+++ src.sh
@@ -6,10 +6,7 @@ FULL_VERSION=$(tail -1 VERSION)
 ###
 function checkout(){
     $1 $2
-    cd $2
-#    omake check || exit 1
-#    omake clean
-    cd -
+#    (cd $2; omak check || exit 1; omake clean)
 }
 echo '### creating start... ###'
 echo $VERSION
@@ -22,23 +19,30 @@ rm -rf habc-*
 mkdir $dir
 
 echo '### checkout(xml) ###'
-checkout 'svn export http://www.libspark.org/svn/ocaml/abc2xml/trunk' ${dir}/xml || exit 1
+checkout 'svn export http://www.libspark.org/svn/ocaml/abc2xml/trunk' ${dir}/xml
 
 echo '### checkout(core) ###'
-checkout 'git clone git@github.com:mzp/scheme-abc.git --depth=0' ${dir}/scm || exit 1
-rm -rf ${dir}/core/.git
+checkout 'git clone git@github.com:mzp/scheme-abc.git --depth=0' scm
+cp -r scm ${dir}
+rm -rf ${dir}/scm/.git
 
 echo '### copy driver ###'
 sed "s/<Version>/${FULL_VERSION}/" habc > ${dir}/habc
 cp extra/* ${dir}
 
 echo '### install check ###'
-cd $dir
-omake install PREFIX=./test-dist
-ls ./test-dir/bin/habc     || exit 1
-ls ./test-dir/bin/habc-xml || exit 1
-ls ./test-dir/bin/habc-scm || exit 1
+DIST=$PWD/test-dist
+rm -rf $DIST
+(cd $dir;omake install PREFIX=${DIST}; omake clean)
+ls ${DIST}/bin/habc     || exit 1
+ls ${DIST}/bin/habc-xml || exit 1
+ls ${DIST}/bin/habc-scm || exit 1
 
 echo '### tagging ###'
+Tag="v${VERSION}"
+svn copy http://www.libspark.org/svn/ocaml/abc2xml/trunk http://www.libspark.org/svn/ocaml/abc2xml/tags/${Tag} -m "Release $Tag"
+(cd scm; git tag ${Tag}; git push --tags)
+rm -rf scm
+
 echo '### package ###'
-tar cvjf ${dir}.tar.bz2 ${dir}
+tar cvjf ${dir}-src.tar.bz2 ${dir}
